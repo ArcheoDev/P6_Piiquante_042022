36b28944a118efd5c45b67faeddd98037a23f897
ArchéoDev <helene.labit@gmail.com>
2022-04-05 00:00:15 +0200

    étape 1 ok

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+// Mongoose
+
+mongoose.connect('mongodb+srv://ArcheoDev:Africaromana_89!@cluster0.sq37b.mongodb.net/piiquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Express
+
+const app = express();
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
+
+module.exports = app;
