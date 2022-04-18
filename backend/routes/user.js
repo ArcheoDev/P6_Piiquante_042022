ab73819b8509a25c3932c5eafb061ccb4d3e7389
ArchéoDev <helene.labit@gmail.com>
2022-04-18 14:16:53 +0200

    password ok

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,11 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
+const password = require("../middleware/password");
 
 // Router pour la login et le signup
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/signup", password, userCtrl.signup);
+router.post("/login", password, userCtrl.login);
 
 module.exports = router ;
